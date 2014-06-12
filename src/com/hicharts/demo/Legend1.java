7e815a0121e769905a8af756d4c1aa2b0e917d78
swordy <mryangjian@live.com>
2014-06-12 12:07:05 +0800

    创建HiChartsDemo项目
    1. 添加PieChart例子
    2. 添加LegendItem例子

---
+++ src/com/hicharts/demo/Legend1.java
@@ -0,0 +1,12 @@
+package com.hicharts.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Legend1 extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.legenditem1);
+	}
+}
---
+++ src/com/hicharts/demo/PieChart1.java
@@ -0,0 +1,27 @@
+package com.hicharts.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hicharts.widget.PieChart;
+import com.hicharts.widget.PieChartAdapter;
+
+public class PieChart1 extends Activity {
+	private PieChart mPieChart;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.piechart1);
+
+		mPieChart = (PieChart) findViewById(R.id.pieChartView1);
+
+		initChart();
+	}
+
+	private void initChart() {
+		PieChartAdapter adapter = new PieChartAdapter(this, new float[] { 12,
+				32, 34, 24, 23 }, null, R.layout.piechart_pie1);
+		mPieChart.setAdapter(adapter);
+	}
+}
